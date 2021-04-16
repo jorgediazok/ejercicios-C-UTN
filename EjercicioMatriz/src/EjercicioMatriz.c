8029c9fef408417b9c1947b6f3e1e4ac4b643da3
Jorge <jorgediazok@gmail.com>
2021-04-16 00:05:14 -0300

    Ejericico matriz primera parte

---
+++ EjercicioMatriz/src/EjercicioMatriz.c
@@ -0,0 +1,42 @@
+/*
+ ============================================================================
+ Name        : EjercicioMatriz.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define CANTIDAD_CLIENTES 4
+#define MSG_INGRESONOM "Ingrese un nombre: "
+#define MSG_INGRESOAP "Ingrese un apellido: "
+
+int main(void) {
+	setbuf(stdout, NULL);
+
+	int i;
+	char arrayNombres[CANTIDAD_CLIENTES][20];
+	char arrayApellidos[CANTIDAD_CLIENTES][20];
+	char auxNombre[20];
+	char auxApellido[20];
+
+	//Cargar los nombres
+
+	for(i=0;i<CANTIDAD_CLIENTES;i++)
+	{
+		getString(MSG_INGRESONOM, auxNombre);
+
+		strcpy(arrayNombres[i], auxNombre);
+	}
+
+	printArrayStr(arrayNombres, CANTIDAD_CLIENTES);
+
+	//Cargar los apellidos
+
+	return EXIT_SUCCESS;
+}
