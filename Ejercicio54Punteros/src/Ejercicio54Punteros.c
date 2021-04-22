5c2bba31bd8e52b6bd475ed6f1431d180ea71b6c
Jorge <jorgediazok@gmail.com>
2021-04-22 16:37:25 -0300

    Ejercicio 54 punteros

---
+++ Ejercicio54Punteros/src/Ejercicio54Punteros.c
@@ -0,0 +1,38 @@
+/*
+ ============================================================================
+ Name        : Ejercicio54Punteros.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+
+ Elaborar una función que se le pase la dirección de una variable entera e incremente en 1
+ lo apuntado por dicha variable.
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void incrementar(int *pe);
+
+
+int main(void) {
+	setbuf(stdout, NULL);
+
+	int valor = 0;
+	printf("%d \n", valor);
+	incrementar(&valor);
+	printf("%d \n", valor); //Ahora vale 1
+	incrementar(&valor);
+	printf("%d \n", valor); //Ahora vale 2
+
+
+
+	return EXIT_SUCCESS;
+}
+
+void incrementar(int *pe)
+{
+	*pe = *pe +1;
+}
