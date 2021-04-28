459d60751dc356dc41305137ebf3ca056e4d6809
Jorge <jorgediazok@gmail.com>
2021-04-28 15:46:34 -0300

    Le agregue mas funciones pero todavia funciona mal.

--- EjercicioClase21AbrilV2/src/EjercicioClase21AbrilV2.c
+++ EjercicioClase21AbrilV2/src/EjercicioClase21AbrilV2.c
@@ -21,7 +21,11 @@ void inicializarArrayString(char array[][20], int tamano);
 void utn_getArrayInt(int numero[], char *mensaje, char *mensajeError, int minimo, int maximo, int intentos);
 void utn_getArrayChar(char character[], char* mensaje, char*mensajeError);
 void utn_getString(char arrayString[][20], char* mensaje);
-void calcularPromedio(float prom, int nota1, int nota2);
+void calcularPromedio(int arrayUno[], int arrayDos[], float promedio[], int i);
+int mostrarEstudiante(int legajo, char sexo, int edad, int nota1, int nota2, char* apellido, float promedio);
+int mostrarEstudiantes(int* legajo, char* sexo, int* edad, int* nota1, int* nota2, char apellido[][20], float* promedio);
+int ordenarLegajoEstudiantes(int* legajo, char* sexo, int* edad, int* nota1, int* nota2, char apellido[][20], float* promedio);
+int ordenarDobleCriterio(int* legajo, char* sexo, int* edad, int* nota1, int* nota2, float* promedio, char apellido[][20]);
 
 
 int main(void) {
@@ -34,7 +38,6 @@ int main(void) {
 	int nota2[LARGO];
 	float promedio[LARGO];
 	char apellido[LARGO][20];
-	//char auxApellido[LARGO];
 
 	//Inicializar array
 
@@ -45,27 +48,40 @@ int main(void) {
 	inicializarArrayFloat(promedio, LARGO);
 	inicializarArrayString(apellido, LARGO);
 
+	//Cargar data
 	for(int i = 0; i < LARGO ; i++){
 		utn_getArrayInt(&legajo[i], "Ingrese un legajo:", "Error, intente nuevamente\n", 1 , 10000, 3);
 		utn_getArrayInt(&edad[i], "Ingrese una edad:", "Error. Ingrese una edad correcta", 1, 120, 3);
 		utn_getArrayInt(&nota1[i], "Ingrese una nota:", "Error. Ingrese una nota entre 1 y 10", 1, 10, 3);
 		utn_getArrayInt(&nota2[i], "Ingrese una nota:", "Error. Ingrese una nota entre 1 y 10", 1, 10, 3);
+		calcularPromedio(&nota1[i], &nota2[i], &promedio[i], i);
 		utn_getArrayChar(&sexo[i], "Ingrese sexo: f/m \n", "Error. Ingrese sexo correcto");
 		utn_getString(&apellido[i], "Ingrese apellido: \n");
 	}
 
 
-	for(int i = 0; i < LARGO ; i++){
 
-					printf("\n%d legajo: %d ",i , legajo[i]);
-					printf("  sexo: %c " , sexo[i]);
-					printf("  edad: %d " ,edad[i]);
-					printf("  nota 1: %d " ,nota1[i]);
-					printf("  nota 2: %d " ,nota2[i]);
-					printf("  promedio: %.2f " ,promedio[i]);
-					printf("  apellido: %s \n" ,apellido[i]);
+	for(int i = 0; i < LARGO; i++)
+	{
+		mostrarEstudiantes(legajo, sexo, edad, nota1, nota2, apellido, promedio);
+	}
 
-				};
+
+
+	//Imprimir data
+
+		for(int i = 0; i < LARGO ; i++)
+		{
+
+		printf("\n%d legajo: %d ",i , legajo[i]);
+		printf("  sexo: %c " , sexo[i]);
+		printf("  edad: %d " ,edad[i]);
+		printf("  nota 1: %d " ,nota1[i]);
+		printf("  nota 2: %d " ,nota2[i]);
+		printf("  promedio: %.2f " ,promedio[i]);
+		printf("  apellido: %s \n" ,apellido[i]);
+
+		};
 
 	return EXIT_SUCCESS;
 }
@@ -98,19 +114,22 @@ void inicializarArrayString(char array[][20], int tamano){
 
 void utn_getArrayInt(int numero[], char *mensaje, char *mensajeError, int minimo, int maximo, int intentos){
 
-	int bufferInt ; //variable auxiliar;
+		int bufferInt ; //variable auxiliar;
 
 		if (numero != NULL  && mensaje != NULL && mensajeError != NULL && minimo <= maximo && intentos >=0 ){
 				printf("%s \n", mensaje);
 				scanf("%d", &bufferInt);
+				fflush(stdin);
 				if(bufferInt >= minimo && bufferInt <=maximo ){
 					*numero = bufferInt;
 				}else{
 				printf("%s", mensajeError);
+				fflush(stdin);
 				intentos--;
 			};
 		}
 };
+
 
 void utn_getArrayChar(char character[], char* mensaje, char* mensajeError)
 {
@@ -127,10 +146,168 @@ void utn_getString(char arrayString[][20], char* mensaje)
 }
 
 
-void calcularPromedio(float prom, int nota1, int nota2)
+void calcularPromedio(int arrayUno[], int arrayDos[], float promedio[], int i)
 {
-	prom = (float)(nota1 + nota2) / 2;
+	if(arrayUno != NULL && arrayDos != NULL && promedio != NULL)
+	{
+		promedio[i] = ((float)arrayUno[i]+arrayDos[i]) / 2;
+
+	}
 }
 
 
+int mostrarEstudiante(int legajo, char sexo, int edad, int nota1, int nota2, char* apellido, float promedio)
+{
+	printf("\n%d,    %c     %d     %d   %d     %s     %.2f", legajo, sexo, edad, nota1, nota2, apellido, promedio);
 
+	return 0;
+}
+
+int mostrarEstudiantes(int* legajo, char* sexo, int* edad, int* nota1, int* nota2, char apellido[][20], float* promedio)
+{
+	int i;
+	printf("\n legajo    sexo    edad    nota1    nota2    apellido     promedio");
+
+	for(i=0; i < LARGO; i++)
+	{
+		mostrarEstudiante(legajo[i], sexo[i], edad[i], nota1[i], nota2[i], apellido[i], promedio[i]);
+	}
+
+	return 0;
+}
+
+int ordenarLegajoEstudiantes(int* legajo, char* sexo, int* edad, int* nota1, int* nota2, char apellido[][20], float* promedio)
+{
+	int i;
+	int aux;
+	char auxS;
+	char auxA[50];
+	float auxF;
+	int flag = -1;
+
+	while(flag == -1)
+	{
+		flag=0;
+		for(i=0; i<LARGO-1;i++)
+		{
+			if(legajo[i]>legajo[i+1])
+			{
+				aux=legajo[i];
+				legajo[i] = legajo[i+1];
+				legajo[i+1] = aux;
+				auxS = sexo[i];
+				sexo[i] = sexo[i+1];
+				sexo[i+1] = auxS;
+
+				aux= edad[i];
+				edad[i]=edad[i+1];
+				edad[i+1]=aux;
+
+				aux = nota1[i];
+				nota1[i] = nota1[i+1];
+				nota1[i+1] = aux;
+
+				aux = nota2[i];
+				nota2[i] = nota2[i+1];
+				nota2[i+1] = aux;
+
+				strcpy(auxA, apellido[i]);
+				strcpy(apellido[i], apellido[i+1]);
+				strcpy(apellido[i+1], auxA);
+
+				auxF = promedio[i];
+				promedio[i] = promedio[i+1];
+				promedio[i+1] = auxF;
+
+				flag = -1;
+			}
+		}
+	}
+
+	return 0;
+}
+
+
+int ordenarDobleCriterio(int* legajo, char* sexo, int* edad, int* nota1, int* nota2, float* promedio, char apellido[][20])
+{
+	int i;
+	int aux;
+	char auxS;
+	char auxA[20];
+	float auxF;
+	int flag = -1;
+
+	while(flag==1)
+	{
+		flag=0;
+		for(i=0; i<LARGO; i++)
+		{
+			if(promedio[i]>promedio[i+1])
+			{
+				aux = legajo[i];
+				legajo[i] = legajo[i+1];
+				legajo[i+1] = aux;
+
+				auxS = sexo[i];
+				sexo[i] = sexo[i+1];
+				sexo[i+1] = auxS;
+
+				aux = edad[i];
+				edad[i] = edad[i+1];
+				edad[i+1] = aux;
+
+				aux = nota1[i];
+				nota1[i] = nota1[i+1];
+				nota1[i+1] = aux;
+
+				strcpy(auxA, apellido[i]);
+				strcpy(apellido[i], apellido[i+1]);
+				strcpy(apellido[i+1], auxA);
+
+				auxF = promedio[i];
+				promedio[i] = promedio[i+1];
+				promedio[i+1] = aux;
+
+				flag = -1;
+			}
+
+			if(promedio[i] == promedio[i+1])
+			{
+				if(legajo[i] > legajo[i+1])
+				{
+					aux = legajo[i];
+					legajo[i] = legajo[i+1];
+					legajo[i+1] = aux;
+
+					auxS = sexo[i];
+					sexo[i] = sexo[i+1];
+					sexo[i+1] = auxS;
+
+					aux = edad[i];
+					edad[i] = edad[i+1];
+					edad[i+1] = aux;
+
+					aux = nota1[i];
+					nota1[i] = nota1[i+1];
+					nota1[i+1] = aux;
+
+					aux = nota2[i];
+					nota2[i] = nota2[i+1];
+					nota2[i+1] = aux;
+
+					strcpy(auxA, apellido[i]);
+					strcpy(apellido[i], apellido[i+1]);
+					strcpy(apellido[i+1], auxA);
+
+					auxF = promedio[i];
+					promedio[i] = promedio[i+1];
+					promedio[i+1] = auxF;
+
+					flag = -1;
+				}
+			}
+		}
+	}
+
+	return 0;
+}
