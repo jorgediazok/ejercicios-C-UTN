799d5e2f1528ed7d7dac22f3c06cf6c99d7de98a
Jorge <jorgediazok@gmail.com>
2021-04-30 00:08:45 -0300

    Trabajando en el mismo trabajo de la clase

--- EjercicioClase28-4-3-AltaBaja/src/EjercicioClase28-4-3-AltaBaja.c
+++ EjercicioClase28-4-3-AltaBaja/src/EjercicioClase28-4-3-AltaBaja.c
@@ -38,6 +38,9 @@ typedef struct{
 
 int inicializarAlumnos(DatosAlumnos* alumnos, int tamano);
 void menuInicio(int* select);
+void altaAlumnos(DatosAlumnos array[], int posicion);
+int buscarLegajo(DatosAlumnos arrayAlumnos[], int tamanio, int legajo);
+
 
 #define TOTAL_ALUMNOS 5
 
@@ -45,6 +48,10 @@ void menuInicio(int* select);
 int main(void) {
 	setbuf(stdout, NULL);
 
+	DatosAlumnos arrayAlumnos[TOTAL_ALUMNOS];
+	inicializarAlumnos(arrayAlumnos, TOTAL_ALUMNOS);
+
+	int posicion;
 	int opcion;
 	char salir = 'n';
 
@@ -55,6 +62,7 @@ int main(void) {
 		switch(opcion)
 		{
 		case 1:
+			altaAlumnos(arrayAlumnos, posicion);
 			break;
 		case 2:
 			break;
@@ -105,3 +113,50 @@ void menuInicio(int* select)
 	scanf("%d", select);
 
 }
+
+
+void altaAlumnos(DatosAlumnos array[], int posicion)
+
+{
+	if(array[posicion].isEmpty == 0)
+
+	{
+	printf("\nIngrese legajo");
+	scanf("%d", &array[posicion].legajo);
+
+	printf("\nIngrese su sexo");
+	fflush(stdin);
+	scanf("%c", &array[posicion].sexo);
+
+	printf("\nIngrese su edad");
+	scanf("%d", &array[posicion].edad);
+
+	printf("\nIngrese nota 1");
+	scanf("%d", &array[posicion].nota1);
+
+	printf("\nIngrese nota 2");
+	scanf("%d", &array[posicion].nota2);
+
+	printf("\nIngrese su apellido");
+	gets(array[posicion].apellido);
+	}
+
+}
+
+int buscarLegajo(DatosAlumnos arrayAlumnos[], int tamanio, int legajo)
+{
+	int ret = -1;
+
+	for(int i = 0; i < tamanio; i++)
+	{
+		if(arrayAlumnos[i].isEmpty == 0 && arrayAlumnos[i].legajo == legajo)
+		{
+			ret = 1;
+			break;
+		}
+	}
+
+	return ret;
+}
+
+
