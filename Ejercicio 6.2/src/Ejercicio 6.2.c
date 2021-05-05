2808862dfddef8e7e2f901a8623dd34d281df11c
Jorge <jorgediazok@gmail.com>
2021-05-04 22:28:25 -0300

    Ejercicio 6.2

---
+++ Ejercicio 6.2/src/Ejercicio 6.2.c
@@ -0,0 +1,22 @@
+/*
+ ============================================================================
+ Name        : 2.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+
+ Ejercicio 6-2:Realizar un programa que permita el ingreso de 10 números enteros (positivos y negativos).
+ Necesito generar un listado de los números positivos de manera creciente y negativos de manera decreciente.
+ (Como máximo 4 for) Ejemplo: Listado 1 : 4, 5, 6, 10, 18, 29 Listado 2 : -1,-5,-9,-12
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	setbuf(stdout, NULL);
+
+	return EXIT_SUCCESS;
+}
